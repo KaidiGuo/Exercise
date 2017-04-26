3da534588e337999f283884cc0dff5b5b37e5181
Sublunarwind <kaidi.guo@outlook.com>
2017-04-26 21:10:21 +0200

    4-26

--- exercise.py
+++ exercise.py
@@ -31,7 +31,68 @@ def reverseWords_557(s):
     re = " ".join(revers)
     return re
 
+def findComplement_476(num):
+    b = bin(num)[2:]
+    c = ""
+    for i in b:
+        if i == "0":
+            c += "1"
+        else:
+            c += "0"
+    out = int(c,2)
+    return out
+
+
+def findWords_500( words):
+    line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
+    ret = []
+    for word in words:
+        w = set(word.lower())
+        if w.issubset(line1) or w.issubset(line2) or w.issubset(line3):
+            ret.append(word)
+    return ret
+
+def fizzBuzz_412( n):
+    numbers = []
+    for i in range(n):
+        if (i+1)%3 == 0 and (i+1)%5 == 0:
+            numbers.append("FizzBuzz")
+        elif (i+1)%3 == 0:
+            numbers.append("Fizz")
+        elif (i+1)%5 == 0:
+            numbers.append("Buzz")
+        else:
+            numbers.append(str(i+1))
+    return numbers
 
 
 
+def reverseString_344(s):
+    re = s[::-1]
+    return re
 
+def islandPerimeter_463(grid):
+    count = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == 1:
+                if x > 0 and grid[x-1][y] == 1:
+                    count += 0
+                else:
+                    count += 1
+                if y > 0 and grid[x][y-1] == 1:
+                    count += 0
+                else:
+                    count += 1
+                if x +1 < len(grid) and grid[x+1][y] == 1:
+                    count += 0
+                else:
+                    count += 1
+                if y +1 < len(grid[0]) and grid[x][y+1] == 1:
+                    count += 0
+                else:
+                    count += 1
+    return count
+
+test = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+print islandPerimeter_463(test)
